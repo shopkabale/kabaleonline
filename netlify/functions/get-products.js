8d9835d053338c96e9397ea3ecf2d644de6cb733
kabaleonline <shopkabale@gmail.com>
2025-08-15 17:55:55 +0300

    Update get-products.js

--- netlify/functions/get-products.js
+++ netlify/functions/get-products.js
@@ -5,26 +5,17 @@ const matter = require('gray-matter');
 
 exports.handler = async () => {
   try {
-    // This tells the function to look for the '_products' folder
     const postsDirectory = path.join(process.cwd(), '_products');
     const filenames = fs.readdirSync(postsDirectory);
 
-    // Go through each product file, read its content, and get the data
     const products = filenames.map(filename => {
       const filePath = path.join(postsDirectory, filename);
       const fileContents = fs.readFileSync(filePath, 'utf8');
-      
-      // Use gray-matter to parse the file's data
       const { data } = matter(fileContents);
-      
-      // We'll manually create an 'id' from the filename for our links
       data.id = filename.replace(/\.md$/, '');
-
       return data;
     });
 
-    // We no longer need to do filtering here, as the frontend will handle it.
-    // We just send all the products.
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
