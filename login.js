812870969a4fcd73733b0faf88c3a8bc28508c2c
kabaleonline <shopkabale@gmail.com>
2025-08-16 10:50:11 +0300

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-// This is the new login.js file
-document.addEventListener('DOMContentLoaded', () => {
-    // This initializes the widget and places it in our container
-    netlifyIdentity.init();
-    const widgetContainer = document.getElementById('netlify-identity-container');
-    netlifyIdentity.open(); // Open the login modal immediately
-});
