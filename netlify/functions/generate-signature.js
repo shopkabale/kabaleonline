3c21db91e9be725b5ba2dcc78cceacc820955968
kabaleonline <shopkabale@gmail.com>
2025-08-16 12:48:31 +0300

    Update generate-signature.js

--- netlify/functions/generate-signature.js
+++ netlify/functions/generate-signature.js
@@ -1,6 +1,9 @@
 // netlify/functions/generate-signature.js
+
+// This line may need to be installed via package.json
 const cloudinary = require('cloudinary').v2;
 
+// The function uses your secret variables safely on the server
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -9,10 +12,18 @@ cloudinary.config({
 
 exports.handler = async (event) => {
     const timestamp = Math.round(new Date().getTime() / 1000);
+
+    // Create the secure signature
     const signature = cloudinary.utils.api_sign_request({ timestamp }, process.env.CLOUDINARY_API_SECRET);
 
+    // Securely send the signature AND your public keys to the browser
     return {
         statusCode: 200,
-        body: JSON.stringify({ signature, timestamp }),
+        body: JSON.stringify({
+            signature: signature,
+            timestamp: timestamp,
+            cloudname: process.env.CLOUDINARY_CLOUD_NAME,
+            apikey: process.env.CLOUDINARY_API_KEY,
+        }),
     };
 };
